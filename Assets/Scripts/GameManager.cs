668d2f45d03dbdd952db7736672bc2dbfd88498e
Cksgud99 <cksgud1126@gmail.com>
2025-01-23 14:56:44 +0900

    HR-6 자동차 생성 및 도로 생성 기능 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,102 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Prefab
+    [SerializeField] private GameObject carPrefab;
+    [SerializeField] private GameObject roadPrefab;
+    
+    //UI
+    [SerializeField] private MoveButton leftMoveButton;
+    [SerializeField] private MoveButton rightMoveButton;
+    
+    // 도로 오브젝트 풀
+    private Queue<GameObject> _roadPool = new Queue<GameObject>();
+    private int _roadPoolSize = 3;
+    
+    // 도로 이동
+    private List<GameObject> _activeRoads = new List<GameObject>();
+    
+    // Singleton
+    private static GameManager _instance;
+
+    public static GameManager Instance
     {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
+    
+    private void Start()
+    {
+        // Road 오브젝트 풀 초기화
+        InitializeRoadPool();
         
+        StartGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        foreach (var activeRoad in _activeRoads)
+        {
+            activeRoad.transform.Translate(-Vector3.forward * Time.deltaTime);
+        }
     }
+
+    private void StartGame()
+    {
+        // 도로 생성
+        SpawnRoad(Vector3.zero);
+        
+        // 자동차 생성
+        var carController = Instantiate(carPrefab, new Vector3(0, 0, -3f), Quaternion.identity).GetComponent<CarController>();
+        
+        // Left, Right Move Button에 자동차 컨트롤 기능 적용
+        leftMoveButton.OnMoveButtonDown += () => carController.Move(-1f);
+        rightMoveButton.OnMoveButtonDown += () => carController.Move(1f);
+    }
+
+    #region 도로 생성 및 관리
+    /// <summary>
+    /// 도로 오브젝트 풀 초기화
+    /// </summary>
+
+    private void InitializeRoadPool()
+    {
+        for (int i = 0; i < _roadPoolSize; i++)
+        {
+            GameObject road = Instantiate(roadPrefab);
+            road.SetActive(false);
+            _roadPool.Enqueue(road); 
+        }
+    }
+    
+    /// <summary>
+    /// 도로 오브젝트 풀에서 불러와 배치하는 함수
+    /// </summary>
+    
+    public void SpawnRoad(Vector3 position)
+    {
+        if (_roadPool.Count > 0)
+        {
+            GameObject road = _roadPool.Dequeue();
+            road.transform.position = position;
+            road.SetActive(true);
+            
+            _activeRoads.Add(road);
+        }
+
+        else
+        {
+            GameObject road = Instantiate(roadPrefab, position, Quaternion.identity);
+            _activeRoads.Add(road);
+        }
+    }
+    
+    #endregion
 }
+
